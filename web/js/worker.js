c2f8ec71f517db774fa700c51ca79689aa8bef54
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2020-02-05 13:53:07 +0100

    solution to the web-worker

---
+++ web/js/worker.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var curFib = 0;
+
+
+self.onmessage = onMessage;
+
+
+function onMessage(event) {
+	getNextFib()
+}
+
+function getNextFib(){
+	var fibNum = fib(curFib);
+	self.postMessage({ idx:curFib, fib: fibNum});
+	curFib++;
+	setTimeout(getNextFib, 0)
+}
+
+// **********************************
+
+function fib(n) {
+	if (n < 2) {
+		return n;
+	}
+	return fib(n-1) + fib(n-2);
+}
